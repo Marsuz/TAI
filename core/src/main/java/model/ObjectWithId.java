8c75bd8deb8b5e52ba3d048295ff33c642c4dac7
Marcin Zajda <mzajda@guidewire.com>
2016-09-18 18:16:01 +0200

    [new] security setup based on tutorial

--- core/src/main/java/model/ObjectWithId.java
+++ core/src/main/java/model/ObjectWithId.java
@@ -13,7 +13,7 @@ public abstract class ObjectWithId {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    protected Long id;
 
     public Long getId() {
         return id;
---
+++ services/src/main/java/auth/StatelessAuthentication.java
@@ -0,0 +1,24 @@
+package auth;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.social.SocialWebAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import javax.servlet.Filter;
+
+@EnableAutoConfiguration(exclude = { SocialWebAutoConfiguration.class })
+@Configuration
+@ComponentScan
+public class StatelessAuthentication {
+
+	@Bean
+	public Filter characterEncodingFilter() {
+		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+		characterEncodingFilter.setEncoding("UTF-8");
+		characterEncodingFilter.setForceEncoding(true);
+		return characterEncodingFilter;
+	}
+}
