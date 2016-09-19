43fe8c721f2449236ed786f87d90ee77ea5631b0
ArasAwas <seyuioa@gmail.com>
2016-09-19 22:06:41 +0200

    [fix] login update

--- web/src/main/resources/static/app/app.js
+++ web/src/main/resources/static/app/app.js
@@ -1,7 +1,38 @@
 // app.js
 var app = angular.module('app', ['ui.router', 'ngResource', 'ngCookies']);
 
-app.config(function($stateProvider, $urlRouterProvider) {
+app.factory('TokenStorage', function() {
+    var storageKey = 'auth_token';
+    return {
+        store : function(token) {
+            return localStorage.setItem(storageKey, token);
+        },
+        retrieve : function() {
+            return localStorage.getItem(storageKey);
+        },
+        clear : function() {
+            return localStorage.removeItem(storageKey);
+        }
+    };
+}).factory('TokenAuthInterceptor', function($q, $rootScope, TokenStorage) {
+    return {
+        request: function(config) {
+            var authToken = TokenStorage.retrieve();
+            if (authToken) {
+                config.headers['X-AUTH-TOKEN'] = authToken;
+            }
+            return config;
+        },
+        responseError: function(error) {
+            if (error.status === 401 || error.status === 403) {
+                TokenStorage.clear();
+                $rootScope.authenticated = false;
+            }
+            return $q.reject(error);
+        }
+    };
+}).config(function($httpProvider, $stateProvider, $urlRouterProvider) {
+    $httpProvider.interceptors.push('TokenAuthInterceptor');
 
     $urlRouterProvider.otherwise('/');
 
@@ -33,7 +64,7 @@ app.config(function($stateProvider, $urlRouterProvider) {
             templateUrl: 'app/recipes/my-recipes.html',
             controller: 'MyRecipesCtrl',
             onEnter: function($rootScope){
-                    $rootScope.$broadcast('tabChange', 2);
+                $rootScope.$broadcast('tabChange', 2);
             }
         })
 
@@ -57,104 +88,64 @@ app.config(function($stateProvider, $urlRouterProvider) {
             }
         });
 
-
-
-
-
 });
 
-var data;
-var ingredients;
 
-$.getJSON("app/data.json", function(json) {
-    console.log(json); // this will show the info it in firebug console
-    data = json.posts;
-
-});
-
-$.getJSON("app/ingredients.json", function(json) {
-    ingredients = json.ingredients;
-
-});
-
-app.run(['$rootScope', '$state', function($rootScope, $state) {
-
-    $rootScope.$on('$stateChangeStart', function(evt, to, params) {
-        if (to.redirectTo) {
-            evt.preventDefault();
-            $state.go(to.redirectTo, params, {location: 'replace'})
+app.run(function ($rootScope, Authentication, $state, $cookies, TokenStorage, $http) {
+    $rootScope.username = null;
+    // var authCookie = $cookies['AUTH-TOKEN'];
+    // if (authCookie) {
+    //     TokenStorage.store(authCookie);
+    //     delete $cookies['AUTH-TOKEN'];
+    // }
+    // console.log($cookies.get('AUTH-TOKEN'));
+    console.log("getting cookieeee");
+    $http({
+        method: 'GET', url: '/api/user/current', headers: {
+            'X-AUTH-TOKEN': $cookies.get('AUTH-TOKEN')
+        }
+    }).success(function (user) {
+        console.log("success");
+        console.log(user);
+        if (user.username) {
+            $rootScope.authenticated = true;
+            $rootScope.username = user.username;
+            console.log("if");
+            $state.go('home');
+            // $rootScope.token = JSON.parse(atob(TokenStorage.retrieve().split('.')[0]));
+        } else {
+            // console.log("else");
+            console.log("no cuccesss cookie");
+            $rootScope.authenticated = false;
+            $state.go('login');
         }
     });
-}]);
-
-
-
-app.factory('TokenStorage', function() {
-    var storageKey = 'auth_token';
-    return {
-        store : function(token) {
-            return localStorage.setItem(storageKey, token);
-        },
-        retrieve : function() {
-            return localStorage.getItem(storageKey);
-        },
-        clear : function() {
-            return localStorage.removeItem(storageKey);
-        }
-    };
-}).factory('TokenAuthInterceptor', function($q, $rootScope, TokenStorage) {
-    return {
-        request: function(config) {
-            var authToken = TokenStorage.retrieve();
-            if (authToken) {
-                config.headers['X-AUTH-TOKEN'] = authToken;
-            }
-            return config;
-        },
-        responseError: function(error) {
-            if (error.status === 401 || error.status === 403) {
-                TokenStorage.clear();
-                $rootScope.authenticated = false;
-            }
-            return $q.reject(error);
-        }
-    };
-}).config(function($httpProvider) {
-    $httpProvider.interceptors.push('TokenAuthInterceptor');
 });
 
-app.controller('AuthCtrl', function ($scope, $rootScope, $http, $cookies, TokenStorage) {
-    $rootScope.authenticated = false;
-    $scope.token; // For display purposes only
-
-    $scope.init = function () {
-        var authCookie = $cookies['AUTH-TOKEN'];
-        if (authCookie) {
-            TokenStorage.store(authCookie);
-            delete $cookies['AUTH-TOKEN'];
-        }
-        $http.get('/api/user/current').success(function (user) {
-            if (user.username) {
-                $rootScope.authenticated = true;
-                $scope.username = user.username;
-
-                // For display purposes only
-                $scope.token = JSON.parse(atob(TokenStorage.retrieve().split('.')[0]));
-            }
-        });
-    };
-
-    $scope.logout = function () {
+app.controller('LogOutCtrl', function ($rootScope, $cookies, TokenStorage) {
+    $rootScope.logout = function () {
+        // console.log("Logout");
         // Just clear the local storage
         TokenStorage.clear();
         $rootScope.authenticated = false;
-    };
-
-    $scope.getSocialDetails = function() {
-        $http.get('/api/facebook/details').success(function (socialDetails) {
-            $scope.socialDetails = socialDetails;
-        });
+        $rootScope.username = null;
+        $cookies.remove('AUTH-TOKEN');
     };
 });
+
+app.factory('Authentication', function ($resource) {
+    return $resource('/api', {}, {
+        getSocialDetails: {
+            method: 'GET',
+            url: '/api/facebook/details'
+        },
+
+        getCurrent:{
+            method: 'GET',
+            url: '/api/user/current'
+        }
+    })
+});
+
 
 
