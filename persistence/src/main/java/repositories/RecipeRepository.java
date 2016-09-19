b25c9c7fb403c80ba21151911b682e0fc333be35
Marcin Zajda <mzajda@guidewire.com>
2016-09-19 20:28:39 +0200

    [new] added facebook and user controllers

--- persistence/src/main/java/repositories/RecipeRepository.java
+++ persistence/src/main/java/repositories/RecipeRepository.java
@@ -2,7 +2,10 @@ package repositories;
 
 import model.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * Created by Marcin on 2016-06-28.
@@ -10,4 +13,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+    @Query("select rec from Recipe rec order by rec.likeCounter DESC LIMIT 0,20")
+    List<Recipe> findTopRecipes();
 }
